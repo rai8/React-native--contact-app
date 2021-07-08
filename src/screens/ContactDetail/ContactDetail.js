fe93a883f7286790aa009bd2fbe34734dd51c8db
rai8 <hytonne@outlook.com>
2021-07-08 09:53:05 +0300

    setting up context

---
+++ src/screens/ContactDetail/ContactDetail.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const ContactDetail = () => {
+  return (
+    <View>
+      <Text>This is the contact detail screen</Text>
+    </View>
+  );
+};
+
+export default ContactDetail;
---
+++ src/screens/Contacts/Contacts.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const Contacts = () => {
+  return (
+    <View>
+      <Text>This is the contact screen</Text>
+    </View>
+  );
+};
+
+export default Contacts;
